002a24479d46206a49234b28f17ba0280f9c90c5
Filipe Molina <filipemolina@live.com>
2015-09-15 17:40:14 -0300

    Mudanças locais

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Authentication routes...
+Route::get('/login', 'Auth\AuthController@getLogin');
+Route::post('/login', 'Auth\AuthController@postLogin');
+Route::get('/logout', 'Auth\AuthController@getLogout');
 
+// Registration routes...
+Route::get('/register', 'Auth\AuthController@getRegister');
+Route::post('/register', 'Auth\AuthController@postRegister');
+
+// Resourceful Controllers
 Route::resource('usuarios', 'UsersController');
