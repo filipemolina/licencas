60b49cb652ac960b84018076bb282a2aab802cce
Filipe Molina <filipemolina@live.com>
2015-11-24 09:41:15 -0200

    Alterações no PDF

--- resources/views/licencas/pdf.blade.php
+++ resources/views/licencas/pdf.blade.php
@@ -109,6 +109,36 @@
 
 		<div class="condicoes">CONDIÇÕES DE VALIDADE DA LICENÇA</div>
 
+		<div class="condicionantes">
+
+			<ol class="cond" start="4">
+				<li>Requerer a renovação desta licença no mínimo 120 (cento e vinte) dias antes do vencimento do seu prazo de validade, caso a implantação do projeto não seja concluída nesse prazo;</li>
+				<li>Atender à Resolução nº. 001/90 do CONAMA, de 08.03.90, publicada no D.O.U. de 02.04.90, que dispõe sobre critérios e padrões de emissão de ruídos;</li>
+				<li>Promover a limpeza periódica do sistema de fossa - filtro, utilizando os serviços de empresa licenciada pela <span style="font-weight: bold;">INEA – Instituto Estadual do Ambiente ou Prefeitura</span> para tal atividade, mantendo os comprovantes à disposição da fiscalização; </li>
+				<li>Atender a NT 202 – Critérios e Padrões para lançamentos de efluentes líquidos, aprovada pela Deliberação CECA nº. 1.007, de 04.12.86, publicada no D.O.R.J. de 12.12.86; </li>
+				<li>Atender a DZ 942 de 01.08.90 – Diretriz do Programa de Autocontrole de Efluentes, PROCON-ÁGUA, aprovada pela Deliberação CECA nº. 1.995, de 10.10.90, publicada no D.O.R.J. de 14.01.91;</li>
+				<li>Não lançar quaisquer resíduos na rede de drenagem ou nos corpos d'água;</li>
+				<li>Atender à DZ – 215. R-4 – Diretriz de Controle de Carga Orgânica Biodegradável em Efluentes líquidos de origens Sanitárias, aprovadas pela Deliberação CECA nº. 4.886 de 25.09.07 e publicada no D.O.R.J. de 05.10.07;</li>
+				<li>Atender à DZ-1310.R-7 – Sistema de Manifesto de Resíduos, aprovada pela Deliberação CECA 4.497 de 03.09.04 e publicada no D.O.R.J. de 21.09.04;</li>
+				<li>Apresentar a cada três meses, junto à SEMUAM/DLCA, o manifesto de resíduos emitidos por empresa licenciada pelo INEA;</li>
+				<li>Apresentar a cada seis meses, junto à SEMUAM/DLCA, análise de efluentes realizada por empresa credenciada pelo INEA.</li>
+				<li>Não realizar queima de qualquer material ao ar livre;</li>
+				<li>Evitar todas as formas de acúmulo de água que possam propiciar a proliferação do mosquito <span style="font-wight: bold; text-decoration: underline; font-style: italic;">Aedes aegypti</span>, transmissor da dengue;</li>
+				<li>Submeter previamente a SEMUAM/DLCA, para análise e parecer, qualquer alteração no projeto;</li>
+				<li>São de responsabilidade da empresa todas as informações contidas neste processo;</li>
+				<li>A SEMUAM/DLCA exigirá novas medidas de controle, sempre que julgar necessário;</li>
+				<li>Seguir rigorosamente os padrões de segurança determinado pelo Corpo de Bombeiro do Estado do Rio de Janeiro, de acordo com o Laudo de Exigências;</li>
+			</ol>
+			
+		</div>
+
+		<div class="assinatura">
+			<div>_____________________________________________</div>
+			<div>ADAUTO JOSÉ PRADO DOS SANTOS</div>
+			<div class="pequeno">SECRETÁRIO MUNICIPAL DE MEIO AMBIENTE</div>
+			<div class="pequeno">MAT: 60/009.216</div>
+		</div>
+
 	</div>
   
 </body>
